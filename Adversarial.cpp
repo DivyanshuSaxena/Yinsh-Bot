82c3622253c57ecde76cf6c2fcde4159186135b5
Divyanshu Saxena <dssaxena2011@gmail.com>
2018-09-12 16:49:18 +0530

    Added code for slant row of contiguoug markers

--- Adversarial.cpp
+++ Adversarial.cpp
@@ -6,14 +6,12 @@
 
 #include "Board.h"
 
-
-
 double Board::getLinearMarkers() {
     double h;
     int player1 = 4;
     int player2 = 5;
 
-    // Vertical Rows for both opponents
+    // Vertical and Horizontal Rows for both opponents
     int rowskone1 = 0;
     int rowsktwo1 = 0;
     int rowskone2 = 0;
@@ -47,7 +45,7 @@ double Board::getLinearMarkers() {
                 } 
                 prevMarkerVert = config[i][j];
             }
-            
+
             // Horizontal Rows
             if (config[j][i] == prevMarkerHorz) {
                 if (!(prevMarkerHorz == player1 || prevMarkerHorz == player2) ) continue;
@@ -73,6 +71,37 @@ double Board::getLinearMarkers() {
         }
     }
 
+    // Slant Rows
+    for (int diff = -n; diff <= n; diff++) {
+        // Let the slant line be x-y = c, then diff is the iterator for c
+        int startj = (diff<=0) ? 0 : diff;
+        int endj = (diff<=0) ? 10 : (10-diff);
+        int prevMarker = config[startj][startj-diff];
+        int count = 0;
+        for (int j = startj+1; j <= endj; j++) {
+            if (config[j][j-diff] == prevMarker) {
+                if (prevMarker != player1 && prevMarker != player2) continue;
+                count++;
+                if (count == k-2) {
+                    if (prevMarker == player1) rowsktwo1++;
+                    else rowsktwo2++;
+                } else if (count == k-1) {
+                    if (prevMarker == player1) {
+                        rowskone1++;
+                        rowsktwo1--;
+                    } else {
+                        rowskone2++;
+                        rowsktwo2--;
+                    }
+                }
+            } else {
+                if (config[j][j-diff] == player1 || config[j][j-diff] == player2) {
+                    count = 1;
+                } 
+                prevMarker = config[j][j-diff];
+            }
+        }
+    }
     return h;
 }
 
