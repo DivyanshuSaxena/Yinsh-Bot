9c5fcafa804725dc9bd1c9e01483dba4e55ce668
Divyanshu Saxena <dssaxena2011@gmail.com>
2018-09-15 01:29:18 +0530

    Added snippet to track k-in-a-row and accordingly returns the evaluation

--- State.h
+++ State.h
@@ -30,19 +30,30 @@ class State {
         int rowsktwo1, nonFlipRowsktwo1;
         int rowskone2, nonFlipRowskone2;
         int rowsktwo2, nonFlipRowsktwo2;
+
+        int startkx, startky;
+        int endkx, endky;
         bool kInRow;
         double heuristic;
 
-        void incrementkRows(int, bool);
+        void incrementkRows(int, bool, int, int);
         void incrementRows(int, int, bool);
+
     public:
         State(Board* board);
         Board* stboard;
         void setWeight(double weight, int feature);
 
+        // Accessor Methods
+        bool isTerminalNode();
+
+        // Operations
+        void resetFeatures();
+
         // Heuristics
         void getLinearMarkers();
         double weightedSum();
-        bool getEvaluation();
+        bool evaluate(); 
+        double getEvaluation();
 };
 #endif /* State_H */
